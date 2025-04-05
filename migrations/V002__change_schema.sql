b308f7cf9496a9fe08b06e6ac91fe7367bed5bfa
norfeus <norf.nile@gmail.com>
2025-04-06 01:22:54 +0300

    .

--- migrations/V002__change_schema.sql
+++ migrations/V002__change_schema.sql
@@ -1,5 +1,14 @@
-ALTER TABLE product ADD COLUMN price DOUBLE PRECISION;
-ALTER TABLE orders ADD COLUMN date_created DATE DEFAULT current_date;
+ALTER TABLE product
+    ADD price DOUBLE PRECISION,
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE orders
+    ADD date_created DATE,
+    ADD PRIMARY KEY (id);
 
 DROP TABLE product_info;
 DROP TABLE orders_date;
+
+ALTER TABLE order_product
+    ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES product (id),
+    ADD CONSTRAINT fk_orders_id FOREIGN KEY (order_id) REFERENCES orders (id);
